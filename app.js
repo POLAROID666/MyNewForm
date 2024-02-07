9b3b30ecb26930f1f8e720b86ec5bf126acae0b6
POLAROID <polaroid.666@mail.ru>
2024-02-07 12:26:50 +0300

    heheh

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const steps = Array.from(document.querySelectorAll('form .step')) 
 const nextBtn = document.querySelectorAll('form .next-btn')
 const prevBtn = document.querySelectorAll('form .previos-btn')
+const endBtn = document.querySelectorAll('form .end-btn')
 const form = document.querySelector('form')
 
 nextBtn.forEach(button=>{
@@ -27,4 +28,32 @@ function changeStep(btn){
     }
     steps[index].classList.add('active')
     console.log(index)
+}
+
+const inputElement = document.getElementById('DuckName')
+const buttonCreate = document.getElementById('create')
+const stepElement = document.getElementById('step-4')
+
+console.log
+
+buttonCreate.onclick = function () {
+    stepElement.innerHTML = `<div class="form-group">
+    <label for="OneLabel">Что такое утка</label>
+    <span>${inputElement.value}</span>
+    <div class="form-group">
+    <label for="OneLabel">Где они обитают</label> 
+    <span></span>
+    <div class="form-group">
+    <label for="OneLabel">Когда стоит покупать зимную куртку</label> 
+    <span></span>
+    <div class="form-group">
+    <label for="OneLabel">Что такое покупка</label> 
+    <span></span>
+    <div class="form-group">
+    <label for="OneLabel">Я ничего не придумал</label> 
+    <span></span>
+    <div class="form-group">
+    <label for="OneLabel">Какие ананасы ты любишь</label> 
+    <span></span>
+    </div>`
 }
